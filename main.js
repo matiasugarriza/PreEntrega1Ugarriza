1db61f9248c76f4bffcb484295971ead0454b940
Matias Ugarriza <matiasugarriza@gmail.com>
2022-11-27 18:38:40 -0300

    Primer Commit

---
+++ main.js
@@ -0,0 +1,7 @@
+/* CALCULADORA DE SALDOS */
+
+//OBJETIVO: Una aplicación que calcule, a partir de un saldo inicial, los movimientos diferenciando entre Ingreso y Egreso (Condicional). Se podrán cargar diversos movimientos (Ciclo) hasta dar por finalizada la carga. Luego, el programa entregará una lista de movimientos, una suma de Ingresos, una suma de Egresos y el Saldo Final.
+
+let fechaInicial = prompt("Ingrese la fecha Inicial en el siguiente formato dd/mm/aaaa");
+let saldoInicial = parseInt(prompt("Ingrese el saldo Inicial: "));
+
