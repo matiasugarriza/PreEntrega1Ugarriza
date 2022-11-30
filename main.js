2e82635b938343bfc01cf9f19f68171b4842ab39
Matias Ugarriza <matiasugarriza@gmail.com>
2022-11-29 23:25:54 -0300

    preentrega

--- main.js
+++ main.js
@@ -1,46 +1,68 @@
+
+
 /* CALCULADORA DE SALDOS */
 
-//OBJETIVO: Una aplicación que calcule, a partir de un saldo inicial, los movimientos diferenciando entre Ingreso y Egreso (Condicional). Se podrán cargar diversos movimientos (Ciclo) hasta dar por finalizada la carga. Luego, el programa entregará una lista de movimientos, una suma de Ingresos, una suma de Egresos y el Saldo Final.
+//OBJETIVO: Una aplicación que calcule, a partir de un saldo inicial, los movs diferenciando entre Ingreso y Egreso (Condicional). Se podrán cargar diversos movs (Ciclo) hasta dar por finalizada la carga. Luego, el programa entregará una lista de movs, una suma de Ingresos, una suma de Egresos y el Saldo Final.
 
-//Fecha y Saldo Inicial
-let fechaInicial = prompt("Escriba la fecha Inicial en el siguiente formato: dd/mm/aaaa");
-let saldoInicial = parseFloat(prompt("Escriba el saldo Inicial: "));
-console.log("Saldo inicial: " + fechaInicial + " --> $" + saldoInicial);
+//Variables a usar:
+let fechaInicial = 0;
+let saldoInicial = 0;
+let nMov = 0;
+let monto = 0;
+let nuevoMov = 0;
 
-//Tipo de Movimiento
-let tipoMov = prompt("Escriba el tipo de movimiento. Si es un Ingreso escriba '1', si es un Egreso escriba '2'. Para finalizar la carga de datos escriba 'fin'. ");
-//Variables
-let fechaMovIng = 0;
-let movIng = 0;
-let motIng = 0;
-let fechaMovEgr = 0;
-let movEgr = 0;
+//Suma de montos y saldo con arrow function.
+let sumaSaldo = (a,b) => a + b ;
+function resultado() {
+    console.log("Saldo: " + "$" + sumaSaldo(saldoInicial,monto));
+};
 
-//Switch para elegir entre ingreso y egreso:
-while (tipoMov != "fin"){
-    switch (tipoMov){
-        case "1":
-            fechaMovIng = prompt("Escriba la fecha del movimiento en el siguiente formato: dd/mm/aaaa");//Agregar Validación de fechas.
-            movIng = parseFloat(prompt("Escriba el valor de Ingreso: "));
-            while(isNaN(movIng)){
-                movIng = parseFloat(prompt("Solo se permiten números. Escriba el valor de Ingreso: "));
-            };
-            // Incluir un condicional para que sea número.
-            motIng = prompt("Escriba el motivo del movimiento: ");
-            console.log(fechaMovIng + " Ingreso --> $" + movIng + " Detalle: " + motIng);
-            break;
-        case "2":
-            fechaMovEgr = prompt("Escriba la fecha del movimiento en el siguiente formato: dd/mm/aaaa");//Agregar Validación de fechas.
-            movEgr = parseFloat(prompt("Escriba el valor de Egreso: "));
-            while(isNaN(movEgr)){
-                movEgr = parseFloat(prompt("Solo se permiten números. Escriba el valor de Ingreso: "));
-            };
-            motEgr = prompt("Escriba el motivo del movimiento: ");
-            console.log(fechaMovEgr + " Egreso --> -$" + movEgr + " Detalle: " + motEgr);
-            break;
+
+/* PASOS DEL PROGRAMA */
+
+
+// 1) Fecha y Saldo Inicial
+function inicio(){
+    fechaInicial = prompt("Escriba la fecha Inicial en el siguiente formato: dd/mm/aaaa");
+    saldoInicial = parseFloat(prompt("Escriba el saldo Inicial: "));
+    while (isNaN(saldoInicial)|| saldoInicial == undefined){
+        saldoInicial = 0;
     }
-    tipoMov = prompt("Escriba el tipo de movimiento. Si es un Ingreso escriba '1', si es un Egreso escriba '2'. Para finalizar la carga de datos escriba 'fin'. ");
+    console.log("Saldo inicial: " + fechaInicial + " --> $" + saldoInicial);
 }
-//Función para realizar la suma del saldo:
-const saldo = (valor1, valor2, valor3) => valor1 + valor2 - valor3;
-console.log("Su saldo actual es de: $"+saldo(saldoInicial,movIng,movEgr)); //
+
+inicio();
+
+// 2) Nuevo Movimiento - Función para realizar nuevos movimientos.
+function mov() {
+    nuevoMov = prompt("¿Desea realizar un nuevo movimiento? Escriba si o no.");
+    Cond();
+};
+
+mov();
+
+//3) Condicional para respuesta a nuevo movimiento.
+//Función que contiene los datos del movimiento.
+function movimiento(){
+    fecha = prompt("Escriba la fecha del movimiento 1 en el siguiente formato: dd/mm/aaaa");
+    monto = parseFloat(prompt("Ingrese el valor: "));
+    while(isNaN(monto)){
+        monto=0;
+    }
+    tipoMov = parseInt(prompt("¿Qué tipo de movimiento es? Escriba 1 si es un Ingreso y 2 si es un Egreso."))
+    let detalle = prompt("Ingrese un detalle sobre el movimiento: ")
+    if (tipoMov != 1 || tipoMov == 2) {
+        monto = - monto;
+    };
+    console.log("Movimiento "+ nMov +": "+fecha+" - Concepto: "+detalle+"-->"+" $"+monto);
+    mov();
+};
+function Cond() {
+    if(nuevoMov == "Si" || nuevoMov == "si" || nuevoMov == "Sí" || nuevoMov == "sí" || nuevoMov == "S" || nuevoMov == "s") {
+        nMov = nMov+1;
+        saldoInicial = sumaSaldo(saldoInicial,monto);
+        movimiento();
+    };
+}
+
+resultado();
